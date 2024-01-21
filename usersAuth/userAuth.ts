c6d065a0f976cf423bebec07ed38224461b77a4c
walid daou <walid.daou2023@gmail.example>
2024-01-21 16:56:56 +0200

    menred el newUser ka Token

--- usersAuth/userAuth.ts
+++ usersAuth/userAuth.ts
@@ -11,45 +11,54 @@ const prisma = new PrismaClient();
 const handleRegister = async (req: any, res: any) => {
     const { name, email, password } = req.body;
     try {
-      const check = await prisma.user.findUnique({
-        where: {
-          email: email,
-        },
-      });
-  
-      if (check) {
-        // User already exists
-        console.log('user already exist')
-       
-        return res.status(500).json({ error: 'User already exists' });
-      }
-  
-  
-      const hashedPassword = await bcrypt.hash(password, 10);
-      const newUser = await prisma.user.create({
-        data: {
-          name,
-          email,
-          hashedPassword,
-        },
-      });
-  
-      console.log(newUser);
-      return res.json(newUser);
+        const check = await prisma.user.findUnique({
+            where: {
+                email: email,
+            },
+        });
 
-      
-    //   if (newUser){
-    //     res.redirect('/Login')
-    //   }
-      
+        if (check) {
+            // User already exists
+            console.log('user already exist')
+            // return res.json('user already exist')
+            return res.status(500).json({ error: 'User already exists' });
+        }
+        // const USER = { name , email , password }
+        const tokenSecret = process.env.TOKEN_SECRET;
+
+        if (!tokenSecret) {
+            console.error('Error: TOKEN_SECRET is not defined in environment variables');
+            process.exit(1); // Exit the process or handle the error appropriately
+        }
+
+
+
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const newUser = await prisma.user.create({
+            data: {
+                name,
+                email,
+                hashedPassword,
+            },
+        });
+        const Token = JWT.sign(newUser, tokenSecret);
+        console.log(Token);
+        return res.json({ Token: Token });
+
+
+        //   if (newUser){
+        //     res.redirect('/Login')
+        //   }
+
     } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Internal Server Error' });
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
     }
-  };
+};
 
-  const handelogin = async (req:any,res:any)=>{
+const handelogin = async (req: any, res: any) => {
 
-  }
+}
 
-  export {handleRegister,handelogin}
+export { handleRegister, handelogin }
