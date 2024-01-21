0eb61a42da4a507ec59a87c59accf76d313d3b7c
walid daou <walid.daou2023@gmail.example>
2024-01-21 19:27:26 +0200

    done register , login , still have to authenticate the token

--- usersAuth/userAuth.ts
+++ usersAuth/userAuth.ts
@@ -8,6 +8,12 @@ import JWT from 'jsonwebtoken';
 const prisma = new PrismaClient();
 
 
+
+
+
+
+
+
 const handleRegister = async (req: any, res: any) => {
     const { name, email, password } = req.body;
     try {
@@ -55,10 +61,54 @@ const handleRegister = async (req: any, res: any) => {
         console.error(error);
         res.status(500).json({ error: 'Internal Server Error' });
     }
+
 };
+
+function authenticatToken (req:any,res:any,next:any){
+
+}
+// async function compare(userPassword: any, hashedPassword: any) {
+//     const rest = await bcrypt.compare(userPassword, hashedPassword)
+//     return rest
+
+// };
 
 const handelogin = async (req: any, res: any) => {
 
-}
 
-export { handleRegister, handelogin }
+    try {
+        const check = await prisma.user.findUnique({
+            where: {
+                email: req.body.email,
+            },
+        });
+
+
+        if (!check) {
+            console.log('User does not exist');
+            return res.status(400).json({ error: 'User does not exist' });
+        }
+
+        const passcheck = await bcrypt.compare(req.body.password, check.hashedPassword);
+
+        if (passcheck) {
+            console.log('Login successful');
+            return res.status(200).json({ message: 'Login successful' });
+        } else {
+            console.log('Invalid password');
+            return res.status(400).json({ error: 'Invalid password' });
+        }
+
+
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+};
+
+
+
+
+
+export { handleRegister, handelogin };
